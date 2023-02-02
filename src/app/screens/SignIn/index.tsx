1ba441a80d20bcc57f97f31c53fee1bcb3408981
Julio Echeverri <julio.echeverri@missionlane.com>
2023-02-02 14:35:51 -0500

    Added navigation structure

--- src/app/screens/SignIn/index.tsx
+++ src/app/screens/SignIn/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ScrollView, Text, TouchableOpacity } from 'react-native';
 import { useTranslation } from 'react-i18next';
 
+import CustomTextInput from '@components/CustomTextInput';
 import { Screens } from '@constants/navigation';
 import { MainStackNavigationProps } from '@interfaces/navigation';
 
@@ -18,6 +19,8 @@ const SignIn = ({ navigation }: SignInProps) => {
   return (
     <ScrollView bounces={false} contentContainerStyle={styles.screen}>
       <Text style={styles.header}>{t('signIn.header')}</Text>
+      <CustomTextInput label="Email" outline />
+      <CustomTextInput label="Password" outline />
       <TouchableOpacity style={styles.button} onPress={handlePressNavigate}>
         <Text style={styles.buttonText}>{t('signIn.navigationButtonLabel')}</Text>
       </TouchableOpacity>
