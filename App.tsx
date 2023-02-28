8e1046c13ada54549ea42089a6bc72ecce6801f4
Julio Echeverri <julio.e.marulanda@gmail.com>
2023-02-28 07:36:17 -0500

    Added custom application icon for iOS and Android versions

--- App.tsx
+++ App.tsx
@@ -19,7 +19,7 @@ const App = () => {
   }, []);
 
   useEffect(() => {
-    setTimeout(() => LottieSplashScreen.hide(), 3500);
+    setTimeout(() => LottieSplashScreen.hide(), 2000);
   }, []);
 
   return (
